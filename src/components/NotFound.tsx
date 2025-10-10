026bc332ac685ba521860087bb6da8c22b47b83b
Ganesh Kantimahanthi <60934037+knganesh97@users.noreply.github.com>
2025-10-10 12:39:04 +0530

    Update NotFound component message to be more descriptive

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NotFound() {
   return (
     <div className="p-8 text-center text-xl font-semibold">
-      Challenge Not Found
+      Content Not Found
     </div>
   );
 }
